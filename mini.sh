e8aaf22da701fd365f696a675de600f0ab5fdb83
Nicolas Vaughan <nivaca@users.noreply.github.com>
2022-08-12 13:37:25 -0500

    Update mini.sh

--- mini.sh
+++ mini.sh
@@ -6,10 +6,8 @@ zcat install-tl-unx.tar.gz | tar xf -
 cd install-tl-*
 sudo perl ./install-tl --no-interaction --scheme=small
 
-export PATH=$PATH:/usr/local/texlive/2022/bin/X86_64-linux
-echo "PATH=$PATH:/usr/local/texlive/2022/bin/X86_64-linux">>/etc/environment
-
-cd /usr/local/texlive/2022/bin/X86_64-linux/
+export PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux
+echo "PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux">>/etc/environment
 
 tlmgr update --self --all
 tlmgr install latexmk
