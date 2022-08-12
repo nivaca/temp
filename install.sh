e814b91d106d1fe5bc25fc9eeddf40bd0cb88b1f
Nicolas Vaughan <nivaca@users.noreply.github.com>
2022-08-12 13:09:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ cd /tmp # working directory of your choice
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat install-tl-unx.tar.gz | tar xf -
 cd install-tl-*
-sudo perl ./install-tl --no-interaction --scheme=minimal
+sudo perl ./install-tl --no-interaction --scheme=basic # vs. small
 
-export PATH=$PATH:/usr/local/texlive/2022/bin/X86_65-linux
+export PATH=$PATH:/usr/local/texlive/2022/bin/X86_64-linux
 
 tlmgr update --self --all
 tlmgr install latexmk
